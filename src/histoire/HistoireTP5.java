95dd3fd2755da23623cbae434f6291d04b2ad3fe
mathisVincent1331 <mathis.vincent@univ-tlse3.fr>
2023-11-13 17:18:42 +0100

    les Samourais

--- src/histoire/HistoireTP5.java
+++ src/histoire/HistoireTP5.java
@@ -8,14 +8,18 @@ public class HistoireTP5 {
 		 Commercant kumi = new Commercant("Kumi", 10);
 		 Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
 		 Ronin roro = new Ronin("Roro", "shochu", 60);
+		 Samourai akimoto = new Samourai("Miyamoto", "Akimoto", "saké", 80);
 		 
-		 marco.faireConnaissanceAvec(roro);
-		 marco.faireConnaissanceAvec(yaku);
-		 marco.faireConnaissanceAvec(chonin);
-		 marco.faireConnaissanceAvec(kumi);
-		 marco.listerConnaissance();
-		 roro.listerConnaissance();
-		 yaku.listerConnaissance();
+		 akimoto.faireConnaissanceAvec(marco);
+		 akimoto.listerConnaissance();
+		 akimoto.boire("thé");
+//		 marco.faireConnaissanceAvec(roro);
+//		 marco.faireConnaissanceAvec(yaku);
+//		 marco.faireConnaissanceAvec(chonin);
+//		 marco.faireConnaissanceAvec(kumi);
+//		 marco.listerConnaissance();
+//		 roro.listerConnaissance();
+//		 yaku.listerConnaissance();
 
 	}
 }
